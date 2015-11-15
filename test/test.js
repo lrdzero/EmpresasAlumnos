7614bf465d87658875ade5063907222049d9d816
lrdzero <elendil.capt.gondor@gmail.com>
2015-11-15 10:05:57 -0800

    Añadidos

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var prueba = require('../lib/server');
+var assert = require("assert");
+
+
+describe("TEST",function(){
+it('Creación de empresas', function(done) {
+    prueba.insertarEmpresa("EMPRESA01",12); 
+    prueba.comprobacion("EMPRESA01",function(err,resultado){
+			assert( assert.equal(resultado, true))});
+    done();
+  });
+});
